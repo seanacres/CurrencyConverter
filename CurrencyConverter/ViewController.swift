6add310cf72ee62a7a93b8c291a9078e564286e3
Sean Acres <seanacres@gmail.com>
2019-06-03 16:33:53 -0400

    Finished mvp scope for project

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -10,11 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    enum CurrencyType {
+        case cad
+        case peso
+    }
+    
+    var currencyType: CurrencyType = .cad
+    
+    @IBOutlet weak var fromCurrencyTextField: UITextField!
+    @IBOutlet weak var toCurrencyTextField: UITextField!
+    @IBOutlet weak var toCurrencyLabel: UILabel!
+    @IBOutlet weak var cadButton: UIButton!
+    @IBOutlet weak var mxnButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func convertButtonPressed(_ sender: Any) {
+        guard let convertAmountString = fromCurrencyTextField.text,
+            let convertAmount = Double(convertAmountString) else { return }
+        
+        toCurrencyTextField.text = String(convert(dollars: convertAmount, to: currencyType))
+    }
+  
+    @IBAction func cadButtonPressed(_ sender: Any) {
+        currencyType = .cad
+        toCurrencyLabel.text = "Currency (CAD)"
+        cadButton.isSelected.toggle()
+        mxnButton.isSelected.toggle()
+    }
+    
+    @IBAction func mxnButtonPressed(_ sender: Any) {
+        currencyType = .peso
+        toCurrencyLabel.text = "Currency (MXN)"
+        cadButton.isSelected.toggle()
+        mxnButton.isSelected.toggle()
+    }
+    
+    func convert(dollars: Double, to unit: CurrencyType) -> Double {
+        if unit == .cad {
+            return dollars * 1.35
+        } else {
+            return dollars * 19.78
+        }
+    }
+    
 }
 
